e23083b5764790ec846468ba4793b77def2c912e
dmotz <motzdc@gmail.com>
2011-10-30 01:41:21 -0400

    tabs, here are some spaces

--- holdTheLine.js
+++ holdTheLine.js
@@ -5,25 +5,25 @@
  */
 
 (function($){
-	$(function(){
-		var input = document.createElement('input'),
-			phSupport = 'placeholder' in input;
-		if(phSupport) return;
-		var els = $('[placeholder]');
-		for(var i = 0, len = els.length; i < len; i++){
-			(function($this){
-				if($this.is('input') || $this.is('textarea')){
-					var str = $this.attr('placeholder');
-					$this.val(str)
-					    .bind('focus', function(){
-						    if($this.val() === str) $this.val('');
-						    }
-					    })
-					    .bind('blur', function(){
-						    if($this.val() === '') $this.val(str);
-					    });
-				}
-			})(els.eq(i));
-		}
-	});
+    $(function(){
+        var input = document.createElement('input'),
+            phSupport = 'placeholder' in input;
+        if(phSupport) return;
+        var els = $('[placeholder]');
+        for(var i = 0, len = els.length; i < len; i++){
+            (function($this){
+                if($this.is('input') || $this.is('textarea')){
+                    var str = $this.attr('placeholder');
+                    $this.val(str)
+                        .bind('focus', function(){
+                            if($this.val() === str) $this.val('');
+                            }
+                        })
+                        .bind('blur', function(){
+                            if($this.val() === '') $this.val(str);
+                        });
+                }
+            })(els.eq(i));
+        }
+    });
 })(jQuery);
