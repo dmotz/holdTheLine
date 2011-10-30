161796491d397153b147b0a4ab476151dd38b502
dmotz <motzdc@gmail.com>
2011-10-30 01:35:53 -0400

    slight optimizations

--- holdTheLine.js
+++ holdTheLine.js
@@ -1,34 +1,29 @@
 /*!
- * HoldtheLine: backwards compatibility for HTML5 placeholder support
- * (c) Dan Motzenbecker <motzdc@gmail.com>
+ * HoldtheLine: polyfill for HTML5 placeholder support
+ * Dan Motzenbecker
  * MIT Licensed
  */
 
 (function($){
 	$(function(){
 		var input = document.createElement('input'),
-			placeholderSupport = 'placeholder' in input;
-		if(placeholderSupport){
-			return;
-		}
-		var elements = $('[placeholder]');
-		for(var i = 0, len = elements.length; i < len; i++){
+			phSupport = 'placeholder' in input;
+		if(phSupport) return;
+		var els = $('[placeholder]');
+		for(var i = 0, len = els.length; i < len; i++){
 			(function($this){
 				if($this.is('input') || $this.is('textarea')){
 					var str = $this.attr('placeholder');
-					$this.val(str);
-					$this.bind('focus', function(){
-						if($this.val() === str){
-							$this.val('');
-						}
-					});
-					$this.bind('blur', function(){
-						if($this.val() === ''){
-							$this.val(str);
-						}
-					});
+					$this.val(str)
+					    .bind('focus', function(){
+						    if($this.val() === str) $this.val('');
+						    }
+					    })
+					    .bind('blur', function(){
+						    if($this.val() === '') $this.val(str);
+					    });
 				}
-			})($(elements[i]));
+			})(els.eq(i));
 		}
 	});
 })(jQuery);
